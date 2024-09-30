5eb56dc203d7a0a48b5ba4c7633a165ae92d7b62
noat04 <toannguyen041214@gmail.com>
2024-09-30 11:33:56 +0700

    update

--- Code/BanVeDTHP/src/entity/KhachHang.java
+++ Code/BanVeDTHP/src/entity/KhachHang.java
@@ -28,15 +28,22 @@ public class KhachHang {
 	}
 
 	public void setMaKH(String maKH) {
-		this.maKH = maKH;
+        String regexMaKH = "KH\\d{4}";
+        if(maKH.matches(regexMaKH)) 
+        	this.maKH = maKH;
+        else
+			throw new IllegalArgumentException("Mã khách hàng không hợp lê!");
 	}
-
 	public String getTenKH() {
 		return tenKH;
 	}
 
 	public void setTenKH(String tenKH) {
-		this.tenKH = tenKH;
+		String regexTenKH="^[A-Z][a-z\\p{L}]*([ ]+[A-Z][a-z\\p{L}]*)*$";
+		if(tenKH.matches(regexTenKH))
+			this.tenKH = tenKH;
+		else
+			throw new IllegalArgumentException("Tên khách hàng không hợp lê!");
 	}
 
 	public String getEmail() {
@@ -44,7 +51,11 @@ public class KhachHang {
 	}
 
 	public void setEmail(String email) {
-		this.email = email;
+		String regexEmail = "^[a-zA-Z0-9.]+@gmail\\.com$";
+		if(email.matches(regexEmail))
+			this.email = email;
+		else
+			throw new IllegalArgumentException("Email không hợp lê!");
 	}
 
 	public String getSdt() {
@@ -52,7 +63,11 @@ public class KhachHang {
 	}
 
 	public void setSdt(String sdt) {
-		this.sdt = sdt;
+	    String regexSdt = "^(03|05|07|08|09)\\d{8}$";
+	    if(sdt.matches(regexSdt))
+	    	this.sdt = sdt;
+	    else
+	    	throw new IllegalArgumentException("Số điện thoại không hợp lê!");
 	}
 
 	public String getCccd() {
@@ -60,7 +75,11 @@ public class KhachHang {
 	}
 
 	public void setCccd(String cccd) {
-		this.cccd = cccd;
+		String regexCccd = "^0[0-9]{2}[0-3]\\d{2}\\d{6}$";
+		if(cccd.matches(regexCccd))
+			this.cccd = cccd;
+		else
+			throw new IllegalArgumentException("Căn cước công dân không hợp lê!");
 	}
 
 	@Override
